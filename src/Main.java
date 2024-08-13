4298240ac42425da7ec091778b70c8d0fcbd5036
Bodya170 <Bogdansemenenko54@gmail.com>
2024-08-13 18:46:45 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static int bossHealth = 700;
+    public static int bossDamage = 50;
+    public static String bossDefense; 
+    public static void main(String[] args) {
+
+    }
+}
